fab3a3747d149e354ec1a0e68f421531cd826fdc
Jorge E Gomez <jeg1986@gmail.com>
2023-09-09 19:46:25 -0300

    Prueba de useState armando un contador.

--- pruebas/src/App.js
+++ pruebas/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyButton></MyButton>
       </header>
     </div>
   );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+  function handleClic() {
+    setCount(count + 1);
+  }
+  return (
+    <button className="btn" onClick={handleClic}>
+      Hiciste clic en {count} veces
+    </button>
+  )
 }
 
 export default App;
